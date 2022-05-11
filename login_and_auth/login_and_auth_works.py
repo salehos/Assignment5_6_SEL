8e3eeda7f18b2b75592af5c1dcc8f992362e02fb
amirmosio <amir.mn.mosio@gmail.com>
2022-05-11 14:32:38 +0430

    Login api nad authentication decorator

---
+++ login_and_auth/login_and_auth_works.py
@@ -0,0 +1,63 @@
+import datetime
+import os
+
+import jwt
+from pymongo import MongoClient
+
+mongo_servers = str(os.environ.get('MONGO_SERVERS'))
+mongo_client = MongoClient(mongo_servers)
+
+database = mongo_client.get_database(name="bidood")
+user_col = database.get_collection(name="users")
+
+
+def check_user_credential(username, password):
+    username_check = user_col.find_one({"username": username, "password": password})
+    if username_check is None:
+        return {"res": "Unable to login with provided credential"}
+
+
+def encode_auth_token(username):
+    """
+    Generate auth token
+    """
+    try:
+        payload = {
+            'exp': datetime.datetime.utcnow() + datetime.timedelta(days=1, seconds=0),
+            'iat': datetime.datetime.utcnow(),
+            'username': username,
+        }
+        return jwt.encode(
+            payload,
+            str(os.environ.get('JWT_SECRET_KEY')),
+            alg='HS256'
+        )
+    except Exception as e:
+        return e
+
+
+def decode_auth_token(auth_token):
+    """
+    Validates the auth token
+    """
+    try:
+        payload = jwt.decode(auth_token, str(os.environ.get('JWT_SECRET_KEY')))
+        return payload['username']
+    except jwt.ExpiredSignatureError:
+        return 'Signature expired. Please log in again.'
+    except jwt.InvalidTokenError:
+        return 'Invalid token. Please log in again.'
+
+
+def auth_decorator(func):
+    def wrapper(*args, **kwargs):
+        from flask import request
+        authorization = request.headers.environ['HTTP_AUTHORIZATION']
+        try:
+            jwt_token = authorization.split()[1]
+            username = decode_auth_token(jwt_token)
+            return func(*args, **kwargs, usernmae=username)
+        except:
+            return {"res": "Token has expired or is invalid, please login again"}
+
+    return wrapper
---
+++ login_and_auth/login_flask.py
@@ -0,0 +1,27 @@
+from flask import Flask, request
+
+from login_and_auth_works import check_user_credential, encode_auth_token
+
+app = Flask(__name__)
+
+
+@app.route('/login', methods=['POST'])
+def login_user():
+    user_data = {}
+    try:
+        for m in request.form:
+            user_data.update({m: request.form[m]})
+        check_user = check_user_credential(user_data['username'], user_data['password'])
+        if check_user is not None:
+            return check_user
+        else:
+            return {"res": "Login was successful", "token": encode_auth_token(user_data['username'])}
+    except Exception as e:
+        return {
+            'res': 'failed',
+            'error': str(e)
+        }
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
