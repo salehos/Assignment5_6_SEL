6b23d290822f4e092004977203bc757f412ff754
amirmosio <amir.mn.mosio@gmail.com>
2022-05-20 19:45:19 +0430

    Reformat code

--- register/reg_flask.py
+++ register/reg_flask.py
@@ -1,7 +1,9 @@
-import os
 from flask import Flask, request
+
 from users_works import check_user_exist, create_user
+
 app = Flask(__name__)
+
 
 @app.route('/register', methods=['POST'])
 def register_user():
@@ -9,7 +11,7 @@ def register_user():
     try:
         if request.method == "POST":
             for m in request.form:
-                user_data.update({m : request.form[m]})
+                user_data.update({m: request.form[m]})
             check_user = check_user_exist(user_data['username'], user_data['gmail'])
             if check_user != None:
                 return check_user
@@ -17,10 +19,10 @@ def register_user():
                 return create_user(user_data['username'], user_data['gmail'], user_data['password'])
     except Exception as e:
         return {
-                'res' : 'failed',
-                'error' : str(e)
+            'res': 'failed',
+            'error': str(e)
         }
-    
+
 
 if __name__ == '__main__':
-   app.run(debug = True, port= 3456)
+    app.run(debug=True, port=3456)
--- travel/travel_stuff.py
+++ travel/travel_stuff.py
@@ -1,8 +1,10 @@
-from pymongo import MongoClient
 import os
-from uuid import uuid1
 from datetime import datetime
-mongo_servers=str(os.environ.get('MONGO_SERVERS'))
+from uuid import uuid1
+
+from pymongo import MongoClient
+
+mongo_servers = str(os.environ.get('MONGO_SERVERS'))
 mongo_client = MongoClient(mongo_servers)
 
 database = mongo_client.get_database(name="bidood")
@@ -10,8 +12,8 @@ travel_col = database.get_collection(name="travel")
 user_col = database.get_collection(name="users")
 bike_col = database.get_collection(name="bikes")
 
-
 from math import radians, cos, sin, asin, sqrt
+
 
 def haversine(lon1, lat1, lon2, lat2):
     """
@@ -22,94 +24,99 @@ def haversine(lon1, lat1, lon2, lat2):
     lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
 
     # haversine formula 
-    dlon = lon2 - lon1 
-    dlat = lat2 - lat1 
-    a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
-    c = 2 * asin(sqrt(a)) 
-    r = 6371 # Radius of earth in kilometers. Use 3956 for miles. Determines return value units.
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+    a = sin(dlat / 2) ** 2 + cos(lat1) * cos(lat2) * sin(dlon / 2) ** 2
+    c = 2 * asin(sqrt(a))
+    r = 6371  # Radius of earth in kilometers. Use 3956 for miles. Determines return value units.
     return c * r
+
 
 def get_travel_details(travel_id):
     travel = travel_col.find_one({"travel_id": travel_id})
     if travel == None:
-        return {'res' : 'there is no travel with this travel id'}
+        return {'res': 'there is no travel with this travel id'}
     return travel
+
 
 def create_travel(username, bike_id, x, y):
     is_ongoing = travel_col.find({"rider_username": username, "status": "Ongoing"})
     bike_json = bike_col.find_one({"bike_id": bike_id})
     user_json = user_col.find_one({"username": username})
     if is_ongoing != None:
-        return {'res' : 'you have a travel ongoing! you cant create another'}
+        return {'res': 'you have a travel ongoing! you cant create another'}
     if user_json == None:
-        return {'res' : 'your username is WRONG'}
+        return {'res': 'your username is WRONG'}
     if bike_json['status'] == 'busy':
-        return {'res' : 'this bike is busy'}
+        return {'res': 'this bike is busy'}
     if haversine(bike_json['x'], bike_json['y'], x, y) > 100:
-        return {'res' : 'bike is out of range'}
+        return {'res': 'bike is out of range'}
     travel_id = uuid1()
     travel_data = {
-        "rider_username" : username,
-        "travel_id" : travel_id,
-        "x_start" : bike_json['x'],
-        "y_start" : bike_json['x'],
-        "bike_id" : bike_json['bike_id'],
-        "x_end" : "",
-        "y_end" : "",
-        "distance" : "",
-        "start_date" : str(datetime.now()),
-        "end_date" : "",
-        "status" : "Ongoing",
-        "score" : 0
+        "rider_username": username,
+        "travel_id": travel_id,
+        "x_start": bike_json['x'],
+        "y_start": bike_json['x'],
+        "bike_id": bike_json['bike_id'],
+        "x_end": "",
+        "y_end": "",
+        "distance": "",
+        "start_date": str(datetime.now()),
+        "end_date": "",
+        "status": "Ongoing",
+        "score": 0
     }
     bike_updates = {
         "bike_id": bike_id,
-        "x" : bike_json['x'],
+        "x": bike_json['x'],
         "y": bike_json['y'],
         "type": "busy",
     }
     bike_col.update_one(bike_json, {"$set": bike_updates})
     res = travel_col.insert_one(travel_data)
-    return {'res': 'your travel has been created with id ' + str(travel_id)+ '. hold it for when you want to finish your move.'}
+    return {'res': 'your travel has been created with id ' + str(
+        travel_id) + '. hold it for when you want to finish your move.'}
+
 
 def end_travel(username, travel_id, x, y):
-    travel_data_on_database = travel_col.find_one({'travel_id' : travel_id, 'status': 'Ongoing', 'rider_username': username})
+    travel_data_on_database = travel_col.find_one(
+        {'travel_id': travel_id, 'status': 'Ongoing', 'rider_username': username})
     rider = user_col.find_one({"username": username})
     bike = bike_col.find_one({"bike_id": travel_data_on_database['bike_id']})
-    all_travels = travel_col.find({"distance": { '$gt': 0 }})
+    all_travels = travel_col.find({"distance": {'$gt': 0}})
     all_travels = list(all_travels)
     total_distance_of_all_users = 0
     for t in all_travels:
         total_distance_of_all_users += t['distance']
-    total_distance_of_all_users = float(total_distance_of_all_users)/len(all_travels)
+    total_distance_of_all_users = float(total_distance_of_all_users) / len(all_travels)
     if travel_data_on_database == None:
         return {'res': 'there is no travel with this id and username for rider, maybe it is not ongoing for now!'}
-    distance = haversine(travel_data_on_database['x_start'],travel_data_on_database['y_start'],x,y)
-    score = int(((distance)/total_distance_of_all_users)^2) +1
+    distance = haversine(travel_data_on_database['x_start'], travel_data_on_database['y_start'], x, y)
+    score = int(((distance) / total_distance_of_all_users) ^ 2) + 1
     travel_data = {
-        "rider_username" : username,
-        "travel_id" : travel_data_on_database['travel_id'],
-        "x_start" : travel_data_on_database['x_start'],
-        "y_start" : travel_data_on_database['y_start'],
-        "bike_id" : travel_data_on_database['bike_id'],
-        "x_end" : x,
-        "y_end" : y,
-        "distance" : distance,
-        "start_date" : travel_data_on_database['start_date'],
-        "end_date" : str(datetime.now()),
-        "status" : "Finish",
-        "score" : score
+        "rider_username": username,
+        "travel_id": travel_data_on_database['travel_id'],
+        "x_start": travel_data_on_database['x_start'],
+        "y_start": travel_data_on_database['y_start'],
+        "bike_id": travel_data_on_database['bike_id'],
+        "x_end": x,
+        "y_end": y,
+        "distance": distance,
+        "start_date": travel_data_on_database['start_date'],
+        "end_date": str(datetime.now()),
+        "status": "Finish",
+        "score": score
     }
     bike_data = {
         "bike_id": travel_data_on_database['bike_id'],
-        "x" : x,
+        "x": x,
         "y": y,
         "type": "free",
     }
     user_data = {
         "username": rider['username'],
-        "gmail": rider['gmail'] ,
-        "password": rider['password'] ,
+        "gmail": rider['gmail'],
+        "password": rider['password'],
         "total_score": rider['total_score'] + score,
         "x": x,
         "y": y
@@ -117,7 +124,7 @@ def end_travel(username, travel_id, x, y):
     bike_col.update_one(bike, {"$set": bike_data})
     user_col.update_one(rider, {"$set": user_data})
     travel_col.update_one(travel_data_on_database, {"$set": travel_data})
-    return {'res': 'you finished your travel successfully, your score for this trip is '+ str(score)}
+    return {'res': 'you finished your travel successfully, your score for this trip is ' + str(score)}
 
 
 def get_all_travels(username):
@@ -127,5 +134,5 @@ def get_all_travels(username):
     except:
         travels = None
     if travels == None:
-        return {'res' : 'there is no travel with this travel id'}
+        return {'res': 'there is no travel with this travel id'}
     return travels
